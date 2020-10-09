5777af9d4f7e1ba69fbb0a844076c79811509296
Colin Kish <ckish@hublsoft.com>
2020-10-09 17:00:23 +0100

    PDP 09 Oct 2020

---
+++ gradebook/GradeBook.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            // arrange
+            var x = 5;
+            var book = new Book("");
+            
+            // act
+               
+            // assert
+   
+        }
+    }
+}
---
+++ gradebook/GradeBook/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+
+        public Book(string name)
+        {
+            this.name = name;
+            grades = new List<double>();
+        }
+        
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            result /= grades.Count;
+
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+            Console.WriteLine($"The avarage grade is {result:N1}");
+
+        }
+
+        //Properties usually private
+        private List<double> grades;
+        private string name;
+
+    }
+
+}
