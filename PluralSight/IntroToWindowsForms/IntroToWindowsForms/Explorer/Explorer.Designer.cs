488975f42991ba7295baee2c3e5a4ee97e3536b8
Colin Kish <69456196+ColinKish@users.noreply.github.com>
2021-02-26 17:09:46 +0000

    26 Feb 2021

--- PluralSight/IntroToWindowsForms/IntroToWindowsForms/Explorer/Explorer.Designer.cs
+++ PluralSight/IntroToWindowsForms/IntroToWindowsForms/Explorer/Explorer.Designer.cs
@@ -29,10 +29,16 @@ namespace Explorer
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Explorer));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.LargeImageList = new System.Windows.Forms.ImageList(this.components);
+            this.SmallImageList = new System.Windows.Forms.ImageList(this.components);
+            this.ToolStripLabel = new System.Windows.Forms.Label();
+            this.ViewToolsStripComboBox = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -44,8 +50,8 @@ namespace Explorer
             this.toolStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
-            this.toolStrip1.Size = new System.Drawing.Size(2669, 25);
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(1099, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -53,7 +59,6 @@ namespace Explorer
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 25);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -63,40 +68,74 @@ namespace Explorer
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.listView1);
-            this.splitContainer1.Size = new System.Drawing.Size(2669, 1615);
-            this.splitContainer1.SplitterDistance = 888;
-            this.splitContainer1.SplitterWidth = 10;
+            this.splitContainer1.Size = new System.Drawing.Size(1099, 575);
+            this.splitContainer1.SplitterDistance = 365;
             this.splitContainer1.TabIndex = 1;
             // 
             // treeView1
             // 
             this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
-            this.treeView1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(888, 1615);
+            this.treeView1.Size = new System.Drawing.Size(365, 575);
             this.treeView1.TabIndex = 0;
-            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
             // 
             // listView1
             // 
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.LargeImageList;
             this.listView1.Location = new System.Drawing.Point(0, 0);
-            this.listView1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(1771, 1615);
+            this.listView1.Size = new System.Drawing.Size(730, 575);
+            this.listView1.SmallImageList = this.SmallImageList;
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // LargeImageList
+            // 
+            this.LargeImageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.LargeImageList.ImageSize = new System.Drawing.Size(32, 32);
+            this.LargeImageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // SmallImageList
+            // 
+            this.SmallImageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.SmallImageList.ImageSize = new System.Drawing.Size(16, 16);
+            this.SmallImageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ToolStripLabel
+            // 
+            this.ToolStripLabel.AutoSize = true;
+            this.ToolStripLabel.Location = new System.Drawing.Point(13, 4);
+            this.ToolStripLabel.Name = "ToolStripLabel";
+            this.ToolStripLabel.Size = new System.Drawing.Size(32, 15);
+            this.ToolStripLabel.TabIndex = 2;
+            this.ToolStripLabel.Text = "View";
+            // 
+            // ViewToolsStripComboBox
+            // 
+            this.ViewToolsStripComboBox.FormattingEnabled = true;
+            this.ViewToolsStripComboBox.Items.AddRange(new object[] {
+            "Large Icons",
+            "Small Icons",
+            "Details"});
+            this.ViewToolsStripComboBox.Location = new System.Drawing.Point(52, 4);
+            this.ViewToolsStripComboBox.Name = "ViewToolsStripComboBox";
+            this.ViewToolsStripComboBox.Size = new System.Drawing.Size(121, 23);
+            this.ViewToolsStripComboBox.TabIndex = 3;
+            this.ViewToolsStripComboBox.SelectedIndexChanged += new System.EventHandler(this.ViewToolStripComboBox_SelectedIndexChanged);
             // 
             // Explorer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2669, 1640);
+            this.ClientSize = new System.Drawing.Size(1099, 600);
+            this.Controls.Add(this.ViewToolsStripComboBox);
+            this.Controls.Add(this.ToolStripLabel);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.toolStrip1);
-            this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.Name = "Explorer";
             this.Text = "Explorer Style Application";
             this.Load += new System.EventHandler(this.Explorer_Load);
@@ -115,6 +154,10 @@ namespace Explorer
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList LargeImageList;
+        private System.Windows.Forms.ImageList SmallImageList;
+        private System.Windows.Forms.Label ToolStripLabel;
+        private System.Windows.Forms.ComboBox ViewToolsStripComboBox;
     }
 }
 
--- PluralSight/IntroToWindowsForms/IntroToWindowsForms/Explorer/Explorer.cs
+++ PluralSight/IntroToWindowsForms/IntroToWindowsForms/Explorer/Explorer.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Explorer
 {
@@ -21,58 +20,91 @@ namespace Explorer
 
         private void Explorer_Load(object sender, EventArgs e)
         {
-
-            listView1.Columns.Add("Name", 250);
-            listView1.Columns.Add("Date modified", 150);
-            listView1.Columns.Add("Size", 75, HorizontalAlignment.Right);
-            //ViewToolStripComboBox.SelectedIndex = 0;
-
-            var docs = new TreeNode("My Documents");
-             docs.Tag = Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "hublsoft limited");
-            //docs.Tag = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            treeView1.Nodes.Add(docs);
-            GetFolders(docs);
-            docs.Expand();
-        }
-
-        private void GetFolders(TreeNode node)
-        {
-            var dir = new DirectoryInfo(node.Tag.ToString());
-
-            foreach (var childDir in dir.GetDirectories())
+            try
             {
-                var childNode = new TreeNode(childDir.Name);
-                childNode.Tag = childDir.FullName;
-                node.Nodes.Add(childNode);
-                GetFolders(childNode);
+                listView1.Columns.Add("Name", 250);
+                listView1.Columns.Add("Date modified", 150);
+                listView1.Columns.Add("Size", 75, HorizontalAlignment.Right);
+                ViewToolsStripComboBox.SelectedIndex = 0;
 
+                var docs = new TreeNode("My Documents");
+                docs.Tag = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                treeView1.Nodes.Add(docs);
+
+                GetFolders(docs);
+                docs.Expand();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text);
             }
         }
 
-        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        private void TreeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            
             var dir = new DirectoryInfo(e.Node.Tag.ToString());
 
             listView1.Items.Clear();
-   //         SmallImageList.Images.RemoveByKey(".exe");
-  //          LargeImageList.Images.RemoveByKey(".exe");
-            foreach (var file in dir.GetFiles()) //define variable to hold the current directory path
+            SmallImageList.Images.RemoveByKey(".exe");
+            LargeImageList.Images.RemoveByKey(".exe");
+            foreach (var file in dir.GetFiles())
             {
                 ListViewItem item = new ListViewItem(file.Name);
                 var lastWrite = file.LastWriteTime;
                 item.SubItems.Add(lastWrite.ToShortDateString() + " " + lastWrite.ToShortTimeString());
                 item.SubItems.Add(Math.Ceiling(file.Length / 1024.0) + " KB");
 
-    //            if (!(LargeImageList.Images.ContainsKey(file.Extension)))
-    //            {
-    //                var thisIcon = Icon.ExtractAssociatedIcon(file.FullName);
-    //                SmallImageList.Images.Add(file.Extension, thisIcon);
-    //                LargeImageList.Images.Add(file.Extension, thisIcon);
-    //            }
+                if (!(LargeImageList.Images.ContainsKey(file.Extension)))
+                {
+                    var thisIcon = Icon.ExtractAssociatedIcon(file.FullName);
+                    SmallImageList.Images.Add(file.Extension, thisIcon);
+                    LargeImageList.Images.Add(file.Extension, thisIcon);
+                }
 
                 item.ImageKey = file.Extension;
                 listView1.Items.Add(item);
+            }
+        }
+
+        private void ViewToolStripComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (ViewToolsStripComboBox.Text)
+            {
+                case "Large Icons":
+                    listView1.View = View.LargeIcon;
+                    break;
+                case "Small Icons":
+                    listView1.View = View.SmallIcon;
+                    break;
+                case "Details":
+                    listView1.View = View.Details;
+                    break;
+            }
+        }
+
+        private void GetFolders(TreeNode node)
+        {
+            var dir = new DirectoryInfo(node.Tag.ToString());
+
+            try
+            {
+                foreach (var childDir in dir.GetDirectories())
+                {
+                    if (childDir.Attributes.HasFlag(FileAttributes.Hidden))
+                    {
+                        continue;
+                    }
+
+                    var childNode = new TreeNode(childDir.Name);
+                    childNode.Tag = childDir.FullName;
+                    node.Nodes.Add(childNode);
+
+                    GetFolders(childNode);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text);
             }
         }
     }
