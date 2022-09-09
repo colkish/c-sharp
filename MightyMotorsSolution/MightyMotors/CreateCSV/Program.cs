3707ea003a796ce8a17049b9cf324e11a6417f17
Colin Kish <ckish@hublsoft.com>
2022-09-09 17:14:34 +0100

    might motors CSV

---
+++ MightyMotorsSolution/MightyMotors/CreateCSV/Program.cs
@@ -0,0 +1,49 @@
+﻿// https://code-maze.com/csharp-writing-csv-file/
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace CreateCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PopulateCSV();
+        }
+
+        private static void PopulateCSV()
+        {
+            var CarData = new List<Car>()
+            {
+                 new Car { Make  = "Ford", Model = "Fiesta", Colour = "Yellow", RegYear = 2010, Milleage = 50000, Price=3000, Sold = false }
+                ,new Car { Make  = "Kia", Model = "Sportage", Colour = "White", RegYear = 2019, Milleage = 3000, Price=17000, Sold = false }
+
+            };
+
+            using (var writer = new StreamWriter("C:\\Users\\ckish.HS\\Source\\Repos\\colkish\\c-sharp\\MightyMotors\\MightyMotors\\MightMotorsCarSales.csv")) 
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(CarData);
+            }
+
+            
+        }
+    }
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+        public int RegYear { get; set; }
+        public int Milleage { get; set; }
+        public int Price { get; set; }
+        public bool Sold { get; set; }
+    }
+
+
+}
---
+++ MightyMotorsSolution/MightyMotors/MightyMotors/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CreateCSV;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace MightyMotors
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        { 
+
+            IEnumerable<Car> CarData = ReadSCV();
+
+            //for each and return will only work in here
+            foreach (var car in CarData)
+            {
+                Console.WriteLine(car.Make);  
+            }
+
+        }
+
+        private static IEnumerable<Car> ReadSCV()
+        {
+            var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+            };
+
+            using (var reader = new StreamReader("C:\\Users\\ckish.HS\\Source\\Repos\\colkish\\c-sharp\\MightyMotors\\MightyMotors\\MightMotorsCarSales.csv"))
+            
+            //think I need to do this without a using block
+            using (var csv = new CsvReader(reader, configuration))
+            {
+
+                csv.Context.RegisterClassMap<PersonMap>();
+                var CarData = csv.GetRecords<Car>();
+                return CarData;
+            }
+
+        }
+
+    }
+
+    public class PersonMap : ClassMap<Car>
+    {
+        public PersonMap()
+        {
+            Map(p => p.Make).Index(0);
+            Map(p => p.Model).Index(1);
+            Map(p => p.Colour).Index(2);
+            Map(p => p.RegYear).Index(3);
+            Map(p => p.Milleage).Index(4);
+            Map(p => p.Price).Index(5);
+            Map(p => p.Sold).Index(6);
+
+        }
+
+    }
+}
